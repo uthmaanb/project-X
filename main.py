31dbc8e5fd73cb3afff663457fc74d50160b5d64
Uthmaan <uthmaanbreda@gmail.com>
2021-04-09 11:24:14 +0200

    added new file

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ print("Welcome to Git")
 print("now go home")
 
 name = input("What is your Name? ")
+name = name.strip()
+
+print(f"Hello {name}")
+print("Would you like to know your fitness level?")
